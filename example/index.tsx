4a10918f00b3e6955cc0eea209af3aec8586396a
kacluk123 <sakuy124@gmail.com>
2022-02-08 18:44:31 +0100

    added gap functionality, and container size

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,30 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Carousel } from '../src/EasyCarousel';
 
 const App = () => {
+  const [ index, setIndex ] = React.useState(0)
+
   return (
-    <div>
-      <Thing />
+    <div style={{width: "800px", border: "1px solid red"}}>
+      <Carousel
+        currentIndex={index}
+        changeCurrentIndex={(index) => { setIndex(index) }}
+      >
+        <div style={{width: '400px', height: "200px", background: 'pink'}}>
+          dasdas
+        </div>
+        <div style={{width: '276px',  height: "200px", background: 'green'}}>
+          dasdas
+        </div >
+        <div style={{width: '200px', height: "200px", background: 'blue'}}>
+          dasdas
+        </div>
+        <div style={{width: '500px',  height: "200px", background: 'red'}}>
+          dasdas
+        </div>
+      </Carousel>
     </div>
   );
 };
